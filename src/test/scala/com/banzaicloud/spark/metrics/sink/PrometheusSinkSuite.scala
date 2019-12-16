f5ef16a809f1d9d19b8d7e69341230553cc04d09
Mateusz Zakarczemny <mateusz.zakarczemny@onzo.com>
2019-12-16 23:08:10 +0100

    NO-TICKET - fix namespace substitution

--- src/test/scala/com/banzaicloud/spark/metrics/sink/PrometheusSinkSuite.scala
+++ src/test/scala/com/banzaicloud/spark/metrics/sink/PrometheusSinkSuite.scala
@@ -5,33 +5,37 @@ import java.util
 import java.util.Properties
 import java.util.concurrent.CopyOnWriteArrayList
 
+import com.banzaicloud.spark.metrics.sink.PrometheusSink.SinkConfig
 import com.codahale.metrics.MetricRegistry
 import io.prometheus.client.CollectorRegistry
 import io.prometheus.client.exporter.PushGateway
-import org.apache.spark.SparkConf
+import org.apache.spark.banzaicloud.metrics.sink.{PrometheusSink => SparkPrometheusSink}
 import org.junit.{Assert, Test}
 
 import scala.collection.JavaConverters._
 import scala.util.Try
 
 class PrometheusSinkSuite {
+  case class TestSinkConfig(metricsNamespace: Option[String],
+                            sparkAppId: Option[String],
+                            sparkAppName: Option[String],
+                            executorId: Option[String]) extends SinkConfig
+
+  private val properties = new Properties
+  properties.setProperty("enable-jmx-collector", "true")
+  properties.setProperty("labels", "a=1,b=22")
+  properties.setProperty("period", "1")
+  properties.setProperty("group-key", "key1=AA,key2=BB")
+  properties.setProperty("jmx-collector-config", "/dev/null")
+
   trait Fixture {
+    def sinkConfig = TestSinkConfig(Some("test-job-name"), Some("test-app-id"), Some("test-app-name"), None)
     lazy val pgMock = new PushGatewayMock
     lazy val registry = new MetricRegistry
-    val sparkConf = new SparkConf(true)
-      .set("spark.app.id", "test-app-id")
-      .set("spark.app.name", "test-app-name")
-      .set("spark.metrics.namespace", "test-job-name")
-    val properties = new Properties
-    properties.setProperty("enable-jmx-collector", "true")
-    properties.setProperty("labels", "a=1,b=22")
-    properties.setProperty("period", "1")
-    properties.setProperty("group-key", "key1=AA,key2=BB")
-    properties.setProperty("jmx-collector-config", "/dev/null")
 
-    def withSink[T](fn: (PrometheusSink) => T): Unit = {
+    def withSink[T](fn: (SparkPrometheusSink) => T): Unit = {
       // Given
-      val sink = new PrometheusSinkImpl(properties, registry, pgMock, sparkConf)
+      val sink = new SparkPrometheusSink(properties, registry, null, sinkConfig, _ => pgMock)
       try {
       //When
         sink.start()
@@ -45,8 +49,7 @@ class PrometheusSinkSuite {
   @Test
   def testSinkForDriver(): Unit = new Fixture {
     //Given
-    sparkConf
-      .set("spark.executor.id", "driver")
+    override val sinkConfig = super.sinkConfig.copy(executorId = Some("driver"))
 
     registry.counter("test-counter").inc(3)
     withSink { sink =>
@@ -66,9 +69,7 @@ class PrometheusSinkSuite {
   @Test
   def testSinkForExecutor(): Unit = new Fixture {
     //Given
-    sparkConf
-      .set("spark.executor.id", "executor")
-      .set("spark.executor.id", "2")
+    override val sinkConfig = super.sinkConfig.copy(executorId = Some("2"))
 
     registry.counter("test-counter").inc(3)
 
@@ -93,15 +94,7 @@ class PrometheusSinkSuite {
     }
   }
 
-  class PrometheusSinkImpl(property: Properties,
-                            registry: MetricRegistry,
-                            pushGateway: PushGateway,
-                            sparkConf: SparkConf)
-    extends PrometheusSink(property, registry, _ => pushGateway, sparkConf) {
-
-  }
-
-  class PushGatewayMock extends PushGateway(new java.net.URL("http://example.com")) {
+  class PushGatewayMock extends PushGateway("anything") {
     val requests = new CopyOnWriteArrayList[Request]().asScala
     case class Request(registry: CollectorRegistry, job: String, groupingKey: util.Map[String, String], method: String)
 
